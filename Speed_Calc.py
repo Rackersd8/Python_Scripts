18b49e213c76cb7fb17e954beeca84ee801f302c
Rackersd8 <89323117+Rackersd8@users.noreply.github.com>
2022-09-09 13:13:15 -0700

    Add files via upload

---
+++ Speed_Calc.py
@@ -0,0 +1,24 @@
+import pyfiglet
+import time
+
+
+pyfiglet.print_figlet("Download Speed Calculator")
+
+
+connection_speed = float(input("Enter results from online speed test: "))
+
+calc_num = 8
+
+download_speed = float(connection_speed) / float(calc_num)
+
+if connection_speed < 0:
+    print("You entered a negative integer which cannot be converted. ")
+    exit()
+elif connection_speed < 10:
+    print("You entered", connection_speed, ", please enter any integer above 10. As anything below it will result in less than 1 MB/s sum. ")
+    exit()
+print("\nYour download speed is", download_speed, "MB/s\n\nThis converter will close automatically in 30 seconds....")
+
+time.sleep(30)
+
+exit()
